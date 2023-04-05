86fa7d1aabe9af86bcb26d30c3936b23b20ba82d
abby <ba40431@gmail.com>
2023-04-05 13:14:31 +0800

    golang start

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Golang")
+}
+
+// 撰寫程式 > 建置(build) > 執行程式
+// 建置: go build 程式檔案的名稱
+// 執行程式: 輸入執行程式的檔名
+
+// go run 程式檔案的名稱 -> 幫你同時編譯跟執行
